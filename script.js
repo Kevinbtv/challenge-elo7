9c1fba156e01ab0c76d4f8071c197fac58793673
Kevin Tavares <kevintavaresdev@gmail.com>
2022-03-18 19:53:14 -0300

    atualizações finais

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
+// #Ínicio Código JavaScript
+
+// Seletores
 const openMenu = document.querySelector(".open");
 const closeMenu = document.querySelector(".close");
 const navMobile = document.querySelector(".navMobile");
 const vagasDev = document.querySelector(".sec_dev");
 
+// Função para fechar o Menu Mobile
+const closeAll = () => {
+  navMobile.style.top = "-162%";
+  openMenu.style.display = "block";
+  closeMenu.style.display = "none";
+};
+
+// Abrir o Menu
 openMenu.addEventListener("click", () => {
   navMobile.style.top = "69px";
   closeMenu.style.display = "block";
   openMenu.style.display = "none";
 });
 
+// Fechar o Menu
 closeMenu.addEventListener("click", () => {
-  navMobile.style.top = "-162%";
-  openMenu.style.display = "block";
-  closeMenu.style.display = "none";
+  closeAll();
 });
 
+// Fechar o Menu ao clicar em cada opção
+document.querySelectorAll(".li_mob").forEach(listas => {
+  listas.addEventListener("click", () => {
+    closeAll();
+  });
+});
+
+// Consumo da API para extração dos dados da Vaga
 (async () => {
   const url = await fetch("http://www.mocky.io/v2/5d6fb6b1310000f89166087b");
   const data = await url.json();
   const vagas = data.vagas;
 
   vagas.map(dados => {
+    // Condicional para aparecer apenas as vagas ativas
     if (dados.ativa) {
+      // Seletores das informações
       const cargo = dados.cargo;
       const link = dados.link;
-      const bairro = dados.localizacao?.bairro ?? "Remoto";
-      const cidade = dados.localizacao?.cidade ?? "Remoto";
-      const pais = dados.localizacao?.pais ?? "Remoto";
+      const bairro = dados.localizacao?.bairro;
+      const cidade = dados.localizacao?.cidade;
+      const pais = dados.localizacao?.pais;
+      const check = dados.localizacao
+        ? `<p>${bairro} - ${cidade}, ${pais}</p> `
+        : "<p>Remoto</p>"; //Caso não haja localização, retornar Remoto
 
       const html = `
         <div class="vagas">
           <p><a href="${link}" target="_blank">${cargo}</a></p>
-          <p>${bairro} - ${cidade}, ${pais}</p>
+          ${check}
         </div>
     `;
-
+      // Adicionar o código no HTML
       vagasDev.insertAdjacentHTML("beforeend", html);
     }
   });
